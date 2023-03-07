df472f0e8a95d951d40f2156c923bf6f0056bdc4
Szymon Ogniewski <szymonogniewski00@gmail.com>
2023-03-07 14:02:39 +0100

    fixed routes for ghpages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,8 +9,8 @@ import { PageNotFound } from 'pages/PageNotFound/PageNotFound';
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
+      <Route path="/goit-react-hw-05-movies" element={<Layout />}>
+        <Route path="/goit-react-hw-05-movies" element={<Home />} />
         <Route path="movies" element={<Movies />}>
           <Route path=":id" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
